9d71e7a87e77eabf8f860b7d2e1da3ce8a50c410
NeverMind109 <vladgladkih123@gmail.com>
2021-11-24 01:03:14 +0300

    Created: Added adaptive response

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const flowersSlider = new Swiper('.flowers-slider', {
     // Параметры слайдера
   loop: true,
-  slidesPerView: 6,
   speed: 100,
   keyboard: {
      enabled: true,
@@ -13,6 +12,22 @@ const flowersSlider = new Swiper('.flowers-slider', {
     nextEl: '.flowers-slider__button--next',
     prevEl: '.flowers-slider__button--prev',
   },
+
+  // Настройки для адаптивной версии
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 6,
+    }
+  }
 });
 
 const reviewsSlider = new Swiper('.reviews-slider', {
