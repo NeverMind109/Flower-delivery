a77592e1cebc28d665f275bab3e6e192afcffea1
NeverMind109 <vladgladkih123@gmail.com>
2021-11-23 14:50:52 +0300

    Created: Added Reviews slider

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ const flowersSlider = new Swiper('.flowers-slider', {
       prevEl: '.swiper-button-prev',
     },
   });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 1,
+
+    // Стрелки навигации
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
