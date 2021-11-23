586ae8b35dba8fe4c3126d6fbe85b96dcbc0f027
NeverMind109 <vladgladkih123@gmail.com>
2021-11-23 17:04:12 +0300

    Created: Added arrows sliders & advantages block

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 //  Инициализируем первый slider 
 const flowersSlider = new Swiper('.flowers-slider', {
     // Параметры слайдера
-    loop: true,
-    slidesPerView: 6,
+  loop: true,
+  slidesPerView: 6,
+  speed: 100,
+  keyboard: {
+     enabled: true,
+  },
 
-    // Стрелки навигации
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  const reviewsSlider = new Swiper('.reviews-slider', {
-    // Параметры слайдера
-    loop: true,
-    slidesPerView: 1,
+  // Стрелки навигации
+  navigation: {
+    nextEl: '.flowers-slider__button--next',
+    prevEl: '.flowers-slider__button--prev',
+  },
+});
 
-    // Стрелки навигации
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Параметры слайдера
+  loop: true,
+  slidesPerView: 1,
+  speed: 400,
+
+  // Стрелки навигации
+  navigation: {
+    nextEl: '.review-slider__button--next',
+    prevEl: '.review-slider__button--prev',
+  },
+});
