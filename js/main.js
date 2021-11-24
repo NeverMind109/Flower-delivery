5f4bc9d7fc9c8ad057d0a089e042caf6b1da981b
NeverMind109 <vladgladkih123@gmail.com>
2021-11-24 14:53:34 +0300

    Created: Added fancybox gallery for reviews

--- js/main.js
+++ js/main.js
@@ -1,19 +1,54 @@
-//  Инициализируем первый slider 
+$(document).ready(function() {
+  // fancybox
+Fancybox.bind('[data-fancybox="gallery"]', {
+  animated: false,
+  showClass: false,
+  hideClass: false,
+
+  Toolbar: false,
+
+  closeButton: "top",
+  click: false,
+  dragToClose: false,
+
+  Image: {
+    // Disable animation from/to thumbnail on start/close
+    zoom: true,
+
+    // Disable zoom on scroll event
+    wheel: false,
+
+    // Disable zoom on image click
+    click: false,
+
+    // Fit image horizontally only
+    fit: "contain-w",
+  },
+  Carousel: {
+    Navigation: {
+      prevTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 5l-7 7 7 7"/><path d="M4 12h16"/></svg>',
+      nextTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg>',
+    },
+  },
+});
+
+
+  //  Инициализируем первый slider 
 const flowersSlider = new Swiper('.flowers-slider', {
-    // Параметры слайдера
+// Параметры слайдера
   loop: true,
   speed: 100,
   keyboard: {
-     enabled: true,
+    enabled: true,
   },
-
-  // Стрелки навигации
+// Стрелки навигации
   navigation: {
     nextEl: '.flowers-slider__button--next',
     prevEl: '.flowers-slider__button--prev',
-  },
-
-  // Настройки для адаптивной версии
+},
+// Настройки для адаптивной версии
   breakpoints: {
     // when window width is >= 320px
     320: {
@@ -31,14 +66,15 @@ const flowersSlider = new Swiper('.flowers-slider', {
 });
 
 const reviewsSlider = new Swiper('.reviews-slider', {
-  // Параметры слайдера
-  loop: true,
-  slidesPerView: 1,
-  speed: 400,
-
-  // Стрелки навигации
-  navigation: {
-    nextEl: '.review-slider__button--next',
-    prevEl: '.review-slider__button--prev',
-  },
+// Параметры слайдера
+loop: true,
+slidesPerView: 1,
+speed: 400,
+// Стрелки навигации
+navigation: {
+  nextEl: '.review-slider__button--next',
+  prevEl: '.review-slider__button--prev',
+},
 });
+
+})
